a62ec0b0de2017138e091e26677c08eb786e60d5
Tomek3 <tomaszfielek@tlen.pl>
2017-03-28 23:42:30 +0200

    rest service init

---
+++ src/com/service/jersey/Constants.java
@@ -0,0 +1,9 @@
+package com.service.jersey;
+
+public class Constants {
+	public static String dbClass = "com.mysql.jdbc.Driver";
+    private static String dbName= "reservationservice";
+    public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
+    public static String dbUser = "root";
+    public static String dbPwd = "password";
+}
